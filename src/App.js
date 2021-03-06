b455c25eb4e6fc9078c0c13486688439edfae077
ksoftdev <kuntursoftdev@gmail.com>
2021-03-06 12:36:37 -0500

    CheckOut Process P1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceorderScreen from './screens/PlaceorderScreen'
 
 function App() {
   return (
@@ -20,6 +23,9 @@ function App() {
           <Route path='/login' component={LoginScreen} exact/>
           <Route path='/register' component={RegisterScreen} exact/>
           <Route path='/profile' component={ProfileScreen} exact/>
+          <Route path='/shipping' component={ShippingScreen} exact/>
+          <Route path='/payment' component={PaymentScreen} exact/>
+          <Route path='/placeorder' component={PlaceorderScreen} exact/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
         </Container>
